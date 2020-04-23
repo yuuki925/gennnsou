ae6e62f5bd5352379c3ae2c95ec73bd2c96834cd
gennsou <ichigo5932@gmail.com>
2020-04-23 13:37:57 +0900

    test

---
+++ js/jspractice.js
@@ -0,0 +1,42 @@
+// document.write("Hello World!");
+// alert("Hello World!");
+
+// console.log("Hello World!" + "田中さん");
+// console.log(10 + 10);
+// console.log("10" + "10");
+
+// console.log(1 + 1);
+// console.log(1 - 1);
+// console.log(2 * 2);
+// console.log(10 / 3);
+// console.log(10 % 3);
+// console.log(10 % 3);
+
+// let x = 1;
+// let y = 1;
+// x++; //x = x + 1
+// console.log(x);
+// y--; //y = y - 1
+// console.log(y);
+
+// let x = 7;
+// let y = x++; //xがyに代入されてからインクリメント
+// let x2 = 7;
+// let y2 = ++x2; //x2のインクリメント後にy2に代入
+
+// console.log(x);
+// console.log(y);
+// console.log(x2);
+// console.log(y2);
+
+// console.log(x++);
+// console.log(++x);
+
+'use strict';
+
+let age = 30;
+const name = "田中太郎";
+
+age = age + 2
+
+console.log(`${name}さんの年齢は${age}です`);
